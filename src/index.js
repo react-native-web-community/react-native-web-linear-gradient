89627ff47845ca91a6fdb971227974a1424a1d99
anskydev <dev@ansky.sh>
2019-06-13 22:15:15 -0600

    Turn class component into functional

--- src/index.js
+++ src/index.js
@@ -1,36 +1,18 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 
-export default class LinearGradient extends PureComponent {
-  static defaultProps = {
-    start: {
-      x: 0.5,
-      y: 0,
-    },
-    end: {
-      x: 0.5,
-      y: 1,
-    },
-    locations: [],
-    colors: [],
-    useAngle: false,
-    angle: 45
-  };
-
-  state = {
-    width: 1,
-    height: 1,
-  };
-
-  measure = ({ nativeEvent }) =>
-    this.setState({
-      width: nativeEvent.layout.width,
-      height: nativeEvent.layout.height,
-    });
-
+const LinearGradient = ({
+  start,
+  end,
+  colors,
+  locations,
+  angle,
+  style,
+  children,
+  ...otherProps
+}) => {
   getColors = () =>
-    this.props.colors
-      .map((color, index) => {
+    colors.map((color, index) => {
         const location = this.props.locations[index];
         let locationStyle = '';
         if (location) {
@@ -40,30 +22,32 @@ export default class LinearGradient extends PureComponent {
       })
       .join(',');
 
-  render() {
-    const {
-      start,
-      end,
-      colors,
-      locations,
-      angle,
-      style,
-      children,
-      ...otherProps
-    } = this.props;
-    return (
-      <View
-        {...otherProps}
-        style={[
-          style,
-          {
-            backgroundImage: `linear-gradient(${angle}deg,${this.getColors()})`
-          },
-        ]}
-        onLayout={this.measure}
-      >
-        {children}
-      </View>
-    );
-  }
+  return (
+    <View
+      {...otherProps}
+      style={[
+        style,
+        {
+          backgroundImage: `linear-gradient(${angle}deg,${this.getColors()})`
+        },
+      ]}
+    >
+      {children}
+    </View>
+  );
 }
+
+LinearGradient.defaultProps = {
+  start: {
+    x: 0.5,
+    y: 0,
+  },
+  end: {
+    x: 0.5,
+    y: 1,
+  },
+  locations: [],
+  colors: [],
+  useAngle: false,
+  angle: 45
+};
